e71fa1878b03659709c8b2ebd14aff6ab6e0ac25
Jillian Domingue <jmdomingue@gmail.com>
2020-08-06 18:32:36 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,10 @@
-function forloop(array) {
-  for (let i = 0; i < 25; i++) {
-  if (i === 1){
-    array.push("I am 1 strange loop.")
-  } else {array.push("I am ${i} strange loops.")
-  }
+function forLoop(array) {
+   for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`)
+     } else {
+       array.push(`I am ${i} strange loops.`)
+     }
   }
   return array;
 }
@@ -11,14 +12,19 @@ function forloop(array) {
 function whileLoop(n) {
   let countdown = n;
 
-  while (countdown > 0) {
-    console.log(--countdown)
-  }
-  return "done";
+    while (countdown > 0) {
+      console.log(--countdown)
+    }
+    return "done";
   }
 
 function doWhileLoop(array) {
   function maybeTrue() {
-    return Math.random() >= 0.5}
+    return Math.random() >= 0.5
   }
+
+  do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+    return array;
 }
